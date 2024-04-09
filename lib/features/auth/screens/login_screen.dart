06290c550a2fe914806a0170d4d75aaa26cdd52e
BetelhemN <betelhemnegash10@gmail.com>
2024-04-09 16:06:33 +0300

    app icon change and signin page type animation added

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gazeta/core/common/loader.dart';
@@ -6,6 +7,7 @@ import 'package:gazeta/core/common/sign_in_us_guest.dart';
 import 'package:gazeta/core/constants/constants.dart';
 import 'package:gazeta/features/auth/controller/auth_controller.dart';
 import 'package:gazeta/responsive/responsive.dart';
+import 'package:typewritertext/typewritertext.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -18,8 +20,19 @@ class LoginScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final isLoading = ref.watch(authControllerProvider);
 
+    final controller = TypeWriterController.fromValue(
+      TypeWriterValue([
+        "Stay Updated!",
+        "Get Informed!",
+        "Join the Discussion!",
+        "Explore News!",
+        "Discover Stories!",
+      ]),
+      duration: const Duration(milliseconds: 100),
+      repeat: true
+    );
+ 
     return Scaffold(
-      //
       appBar: AppBar(
         backgroundColor: Colors.black,
         toolbarHeight: 100,
@@ -32,15 +45,22 @@ class LoginScreen extends ConsumerWidget {
       body: isLoading
           ? const Loader()
           : Column(
-              children: [
-                const SizedBox(height: 30),
-                const Text(
-                  'Dive into anything',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 0.5,
-                  ),
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 25),
+                TypeWriter(
+                  controller: controller,
+                  builder: (context, value) {
+                    return Text(
+                      value.text,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.5,
+                      ),
+                    );
+                  },
                 ),
                 const SizedBox(height: 30),
                 Padding(
