783cdc7e5e4aa4e32a0903ebe9514932cf2d4a80
BetelhemN <betelhemnegash10@gmail.com>
2024-04-23 19:13:11 +0300

    unnecessary comment removed

--- lib/features/home/screens/splash_screen.dart
+++ lib/features/home/screens/splash_screen.dart
@@ -64,39 +64,3 @@ class _SplashScreenState extends State<SplashScreen>
 }
 
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
-// import 'package:lottie/lottie.dart';
-
-// class SplashScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Color(0xFFE4A839), // Set background color to white
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           // Center the Lottie animation
-//           Expanded(
-//             child: Center(
-//               child: Image.asset(
-//                 'assets/images/logo.png', // Replace 'your_image.png' with the path to your image
-//                 height: 500, // Adjust the height as needed
-//                 width: 500, // Adjust the width as needed
-//               ),
-//               // child: SizedBox(
-//               //   width: 500, // Adjust width as needed
-//               //   height: 500, // Adjust height as needed
-//               //   child: Lottie.asset(
-//               //     'assets/lottie/splashscreen.json',
-//               //     // Optionally, you can also specify animation controller and options here
-//               //   ),
-//               // ),
-//             ),
-//           ),
-//           // Text below the Lottie animation
-//         ],
-//       ),
-//     );
-//   }
-// }
