dfbdf07874fbf51a0f1cc3b3deefb2debb442191
BettyyN <betelhemnegash10@gmail.com>
2024-05-01 00:42:30 +0300

    Code Refactoring

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,6 +1,7 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
+/// Show a SnackBar with the provided text.
 void showSnackBar(BuildContext context, String text) {
   ScaffoldMessenger.of(context)
     ..hideCurrentSnackBar()
@@ -11,6 +12,7 @@ void showSnackBar(BuildContext context, String text) {
     );
 }
 
+/// Pick an image file and return the result.
 Future<FilePickerResult?> pickImage() async {
   final image = await FilePicker.platform.pickFiles(type: FileType.image);
   return image;
